d8c1d8dd33b3e36f61936276799ac404417f885a
SemanikCZ11 <128865957+SemanikCZ11@users.noreply.github.com>
2023-03-27 15:19:11 +0200

    Add files via upload

---
+++ MS Windows/Cviceni 2/Projekt 1/Program.cs
@@ -0,0 +1,44 @@
+﻿// Tento zdrojovy kod jsem vypracoval zcela samostatne bez cizi pomoci
+// Neokopiroval jsem ani neopsal jsem cizi zdrojove kody 
+// Nikdo me pri vypracovani neradil 
+// Pokud nektery radek porusuje toto pravidlo je oznacen komentarem
+// NENI MOJE TVORBA
+// Poruseni techto pravidel se povazuje za podvod, ktery lze potrestat VYLOUCENIM ZE STUDIA
+// Lukas Semecky 36991
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt_1            // Logické výrazy hodnoty bool-True/False (4 prezentace -snímek 12)
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x1 = 5;
+            int y1 = 3;
+            bool vyraz1 = x1 > y1;
+
+            Console.WriteLine(vyraz1);
+            Console.WriteLine(x1 > y1);
+            Console.WriteLine("{0} > {1}.......{2}",x1, y1, x1 > y1);
+
+            Console.WriteLine();
+
+            int x2 = 4;
+            int y2 = 4;
+
+            Console.WriteLine(x2 > y2);
+            Console.WriteLine("{0} > {1}.......{2}",x2, y2, x2 > y2);
+            Console.WriteLine("{0} >= {1}.......{2}",x2, y2, x2 >= y2);
+            Console.WriteLine("{0} == {1}.......{2}",x2, y2, x2 == y2);
+
+            Console.ReadKey();
+
+            //Výstupem budou hodnoty True/False podle toho jak je podmínka splněna či nikoliv.
+        }
+    }
+}
---
+++ MS Windows/Cviceni 2/Projekt 2/Program.cs
@@ -0,0 +1,37 @@
+﻿// Tento zdrojovy kod jsem vypracoval zcela samostatne bez cizi pomoci
+// Neokopiroval jsem ani neopsal jsem cizi zdrojove kody 
+// Nikdo me pri vypracovani neradil 
+// Pokud nektery radek porusuje toto pravidlo je oznacen komentarem
+// NENI MOJE TVORBA
+// Poruseni techto pravidel se povazuje za podvod, ktery lze potrestat VYLOUCENIM ZE STUDIA
+// Lukas Semecky 36991
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt_2         // Kod vícenásobného příkazu else if (4 prezentace - snímek 16)
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 2;
+            int y =5;
+            int z = 10;
+
+            if (x < y && y > z) Console.WriteLine("Dobrá Prace první příkaz = TRUE");
+            else if (x > y || z < y) Console.WriteLine("Dobrá Prace druhý příkaz = TRUE");
+            else if (x == z || z == y ) Console.WriteLine("Dobrá Prace třetí příkaz = TRUE");
+            else Console.WriteLine("Spatná práce - všechny příkazy jsou FALSE");
+
+            Console.ReadKey();
+
+            //Je to jednoduchý kod porovnávání pokud se nějaká podmínka vyhodnoti jako true tak se vypíše jako dobrá práce
+            //pokud ne tak projde až k poslednímu else ...
+
+
+        }
+    }
+}
