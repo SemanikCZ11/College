f8b0b2c9b536c19f2e3ee25907a2321bdb687d20
SemanikCZ11 <128865957+SemanikCZ11@users.noreply.github.com>
2023-03-27 18:39:21 +0200

    Add files via upload

---
+++ Introduction to Programming/Homework 2/Hodina 6/Cviceni 6.1/Program.cs
@@ -0,0 +1,101 @@
+﻿// Tento zdrojovy kod jsem vypracoval zcela samostatne bez cizi pomoci
+// Neokopiroval jsem ani neopsal jsem cizi zdrojove kody 
+// Nikdo me pri vypracovani neradil 
+// Pokud nektery radek porusuje toto pravidlo je oznacen komentarem
+// NENI MOJE TVORBA
+// Poruseni techto pravidel se povazuje za podvod, ktery lze potrestat VYLOUCENIM ZE STUDIA
+// Lukas Semecky 36991
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cviceni_6._1
+{
+
+    // Práce s textovými řetezci cviceni 6.1 a) Funkce vracejicí pocet písmen (2 Body)
+    //                                       b) Funkce vracejíci počet slov   (2 body)
+    //                                       c) Funkce vracejici slovo prvniho vyskytu
+    //                                       d) Funkce vracejici nejdelši slovo
+    //                                       e) Funkce vracejici lexikograficky nejvetši slovo 
+    //                                       f) Funkce vracejici  nejčastěji se vyskytující slovo
+    
+    class Program
+    {
+
+        static int PocetPismen(string s)//Funkce vracející počet písmen
+        {
+            int pocet = 0,stejne = 0;
+            int i,j;
+
+            for(i = 0; i < s.Length;i++)//For cyklus pro pole charu ve stringu
+            {
+                if (char.IsLetter(s[i]))// Pokud je Char na dané pozici písmeno vstopim do vnořeneho cyklu
+                {
+                    pocet++;// za každé pismeno zvýším počet o jedno 
+                    stejne = 0;
+
+                    for(j = i; j >= 0;j--)// další cyklus pro kontrolu stejnych písmen vždy od dané pozice kterou směrem dolů
+                        if(j != i)// když jsou stejné pozice nekotroluji
+                        {
+                            if (char.ToLower(s[j]) == char.ToLower(s[i]))// změna charu na malá písmena aby mi kontrolovalo podobnost i s Velkym pismenem
+                                stejne++;
+
+                        }
+                    if (stejne > 0) pocet--;// pokud najdu stejná písmena dekrementuji počet o jeden
+
+                }
+            }
+            return pocet;// vracím konečny výsledek 
+
+        }
+        static int PocetSlov(string s)//Funkce vracející pocet slov
+        {
+            int slov = 0;
+            int i;
+
+            for (i = 0; i < s.Length ; i++)//For cyklus pro pole charu daného stringu
+            {
+                
+                if (char.IsLetter(s[i]) || char.IsPunctuation(s[i]))//Pokud je char písmeno nebo znamenko vstupujeme do cyklu
+                {
+                    if (i == s.Length - 1)// pokud jsme na konci stringu zvedame slovo o 1 a nasledně se cyklus ukonci(musíme snížit o jeden)
+                        slov++;
+
+                    else if (char.IsWhiteSpace(s[i + 1])) // pokud nasleduje mezera po pismenu slovo inkrementuji o jeden
+                        slov++;
+                }
+            }
+            return slov;// Vracím pocet slov 
+
+        }
+         
+
+        static void Main(string[] args)
+        {
+            int x,y,i;
+
+            string s1 ="Polevka pro 2 osoby";
+            i = s1.Length;
+
+            x = PocetPismen(s1);
+            y = PocetSlov(s1);
+           
+
+
+            Console.WriteLine("V zadaném řetězci o delce řetezce :{0}: je {1} ruzných písmen",i, x);
+            Console.ReadLine();
+            Console.WriteLine("V zadaném řetezci jsou {0} slova", y);
+            Console.ReadLine();
+
+
+
+
+
+            Console.ReadLine();
+
+        }
+
+    }
+}
---
+++ Introduction to Programming/Homework 2/Hodina 6/Cviceni 6.3/Program.cs
@@ -0,0 +1,104 @@
+﻿// Tento zdrojovy kod jsem vypracoval zcela samostatne bez cizi pomoci
+// Neokopiroval jsem ani neopsal jsem cizi zdrojove kody 
+// Nikdo me pri vypracovani neradil 
+// Pokud nektery radek porusuje toto pravidlo je oznacen komentarem
+// NENI MOJE TVORBA
+// Poruseni techto pravidel se povazuje za podvod, ktery lze potrestat VYLOUCENIM ZE STUDIA
+// Lukas Semecky 36991
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cviceni_6._3
+{
+    //Cviceni Naše vlastni řetezce 6.3 pracujicí s polem Charu[]  -  A)Funkce lexikografického porovnávání řetežcu (2 body) -Hotovo
+    //                                                               B)Funkce vracející index prvního výskytu hledaného řetežce (2 body) - Hotovo
+    //                                                               C)Funkce vracející podřetězec zadaného řetežce (1 bod) - Hotovo
+    //                                                               D)Funkce vracející řetezec vzniklý vložením řetezce  (2 body) - Hotovo
+    //                                                               E)Funkce vracející řetězec odstraněním podřetězce ze zadaného řetězce 
+    //                                                               F)Funkce  vracející řetezec vzniklí z řetežce a nahrazení všech výskytů
+    class Program
+    {
+
+        static int Porovnani(char[] s3,char[] s4)//lexikograficke  porovnávání dvou řetezcu
+        {
+            int i, j;
+
+            for (i = 0; i < s3.Length; i++)// prvni řetezec 
+            {
+                for (j = i; j < s4.Length;)// druhy řetezec se stejnou hodnotou jako prvni
+                {
+                    if (s3[i] > s4[j]) return 1;// pokud je první řetezec vetší vratí mi 1 
+                    else if (s3[i] < s4[j]) return -1;// pokud je druhy řetezec vetší vrací  -1
+                    else break;// jinak break pro porovnání dalších charu
+                }
+
+            }
+            return 0;// pokud jsou obě slova stejná vrátím  0
+
+            // předpokládáme že vždy najdeme nejakou neschodu pokud by druhý řetezec byl stejný a měl o par charů navíc cyklus by vratil 0 i když by byl druhy řetezec vetší
+        }
+
+        static void Podretezec(char[] s3,int index)// načtení Funkce prvního řetezce a indexu od kterého začínám
+        {
+            int i;
+            Console.Write("Podřetezec prvního řetezce:  ");
+            for (i = index; i <= index + 2; i++)// nastavení i na index a delku o hledaní + 2 a vypsaní
+                Console.Write("{0}", s3[i]);
+            Console.ReadLine();
+        }
+        static void SpojeniRetezcu(char[] s3, char[] s4, int index)// spojení dvou řetezcu od určitého indexu
+        {
+
+            int i,j;
+            Console.Write("Spojeni dvou neuplnych řetezců :  ");
+            for (i = 0; i <= index; i++)// vypsaní prvního řetezce od indexu nula a po zadaný index 
+                Console.Write("{0}", s3[i]);
+
+            for (j = index; j < s4.Length; j++)// připojení druhého řetezce od zadaného indexu až po jeho délku
+                Console.Write("{0}", s4[j]);
+            Console.ReadLine();
+
+        }
+        static int PrvniVyskyt(char []s3,char co)//Hodnota hledaného prvního vyskytu
+        {
+
+            int i;
+
+            for (i = 0; i < s3.Length; i++)//hledání charu v řetezci
+            {
+                if (s3[i] == co)// pokud se zadaná hodnota najde vyskočíme z cyklu a vracíme hodnotu
+                    break;
+                else if (s3[i] != co && i == s3.Length -1)// když se nenajde vracíme -1
+                        return -1;
+                     
+            }
+            return i;
+        }
+        
+        static void Main(string[] args)
+        {
+            int i,x;
+
+            char[] s1 = "Kolotoc".ToCharArray();
+            char[] s2 = "Klobouky".ToCharArray();
+            
+            x = Porovnani(s1, s2);//Funkce pro porovnání
+            Console.WriteLine("Porovnání dvou řetezcu: {0}", x);
+            Console.ReadLine();
+
+            Podretezec(s1, 3);// Funkce pro podretezec
+            SpojeniRetezcu(s1, s2, 4);// spojení obou neuplných retezcu
+            
+            i = PrvniVyskyt(s2, 'o');// Funkce pro výskyt charu a jeho vypsání 
+            Console.WriteLine("Index prvního výskytu je na pozici {0}", i);
+            Console.Read();
+
+            
+        }
+
+
+    }
+}
