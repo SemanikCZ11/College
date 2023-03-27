722e40678334c1c046b8f2fb342f0e96e366ef99
SemanikCZ11 <128865957+SemanikCZ11@users.noreply.github.com>
2023-03-27 18:59:24 +0200

    Add files via upload

---
+++ Programing/Semestr 2-Hodina.5/ConsoleApp1/Program.cs
@@ -0,0 +1,76 @@
+﻿// Tento zdrojovy kod jsem vypracoval zcela samostatne bez cizi pomoci
+// Neokopiroval jsem ani neopsal jsem cizi zdrojove kody 
+// Nikdo me pri vypracovani neradil 
+// Pokud nektery radek porusuje toto pravidlo je oznacen komentarem
+// NENI MOJE TVORBA
+// Poruseni techto pravidel se povazuje za podvod, ktery lze potrestat VYLOUCENIM ZE STUDIA
+// Lukas Semecky 36991
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1 // MergeSort
+{
+    class Program
+    {
+        static void rozdel(int[] pole, int l, int r)//Funkce na rekurzivni voláni a rozdělování pole 
+        {
+            int m;
+
+            if (l < r)
+            {
+                m = (r + l) / 2;// delení pole na pulky
+                rozdel(pole, l, m);// rozdělení na menší kousky
+                rozdel(pole, m + 1, r);
+                slij(pole, l, r, m);// funkce na sleváni dat v poli 
+            }      
+        }
+        static void slij(int[] pole,int l,int r,int m)// funkce pro třídění 
+        {
+            int[] poleB = new int[20];// alokuju jsi nové pole
+            int i=l, j=m+1, k=l;
+
+            while(i<=m && j<=r)// Cyklus že i není vetší než střed a že J není vetší než konec tříděného pole
+            {
+                if (pole[i] >= pole[j])// pro sestupné třídění pro vzestupné akorat otočíme znaménka
+                {
+                    poleB[k] = pole[i];// nakopírujeme na pozici K hodnotu pole I
+                    i++;
+                    k++;
+                }
+                else
+                {
+                    poleB[k] = pole[j];// nakopírujeme na pozici K hodnotu pole J
+                    j++;
+                    k++;
+                }
+            }
+            while(i<=m) // pokud nám ještě zbyly prvky v levé části 
+            {
+                poleB[k] = pole[i];
+                i++;
+                k++;
+            }
+            while (j <= r)// pokud nám ještě zbyly prvky v levé části 
+            {
+                poleB[k] = pole[j];
+                j++;
+                k++;
+            }
+            for (k = l; k <=r; k++)// přepisujeme nové rozstříděné hodnoty z pomocného pole do pole původního
+                pole[k] = poleB[k];
+
+        }
+        static void Main(string[] args)
+        {
+            int i;
+            int[] pole = { 84, 73, 46, 86, 60, 40, 18, 54, 83, 9, 88, 22, 72, 34, 51, 69, 35, 95, 16, 14 };
+            rozdel(pole, 0, pole.Length - 1);
+            for (i = 0; i < pole.Length; i++)// vypis pole
+                Console.WriteLine(pole[i]);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Programing/Semestr 2-Hodina.5/Cviceni 5.2 B/Program.cs
@@ -0,0 +1,52 @@
+﻿// Tento zdrojovy kod jsem vypracoval zcela samostatne bez cizi pomoci
+// Neokopiroval jsem ani neopsal jsem cizi zdrojove kody 
+// Nikdo me pri vypracovani neradil 
+// Pokud nektery radek porusuje toto pravidlo je oznacen komentarem
+// NENI MOJE TVORBA
+// Poruseni techto pravidel se povazuje za podvod, ktery lze potrestat VYLOUCENIM ZE STUDIA
+// Lukas Semecky 36991
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cviceni_5._2_B //QuickSOrt
+{
+    class Program
+    {
+        static void Quick(int[] pole,int l,int r)// Funkce pro Quick sort
+        {
+            int pivot, i, j, tmp;
+
+            if(l<r)
+            {
+                pivot = pole[l];// nastavení pivota na zacatek pole
+                i = l;
+                j = r;
+                
+                while(i<j)
+                {
+                    while(pole[i] > pivot) i++;// Pokud je pivot menší inkrementujeme i
+                    while(pole[j] < pivot) j--;//pokud je pivot vetší dekrementujeme J
+                    //Prohozením docílime vzestupného třídění
+
+                    tmp = pole[i];//Pokud nenastane ani jeden While prohazujeme hodnoty
+                    pole[i] = pole[j];
+                    pole[j] = tmp;//pokracujeme dokud bude I ruzné od J
+                }
+                Quick(pole, l, i - 1);//Rekurze pro nového pivota z levé části od starého pivota
+                Quick(pole, i + 1, r);//Rekurze pro nového pivota z pravé části
+            }
+        }
+        static void Main(string[] args)
+        {
+            int i;
+            int[] pole = { 19, 87, 46, 13, 56, 54, 75, 88, 50, 68, 16, 2, 17, 77, 91, 8, 62, 10, 15, 71 };
+            Quick(pole, 0, pole.Length-1);
+            for (i = 0; i < pole.Length; i++)// vypis pole
+                Console.WriteLine(pole[i]);
+            Console.ReadLine();
+        }
+    }
+}
