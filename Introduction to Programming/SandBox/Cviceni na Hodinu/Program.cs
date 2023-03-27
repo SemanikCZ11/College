416b14d7c04854949d32476b92bae4d1cdd18f8c
SemanikCZ11 <128865957+SemanikCZ11@users.noreply.github.com>
2023-03-27 15:48:52 +0200

    Add files via upload

---
+++ Introduction to Programming/SandBox/Cviceni na Hodinu/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Cviceni_na_Hodinu
+{
+    class Program
+    {
+
+        static void JenomSlova5(string zdroj, string cil)
+        {
+            int znak = 0,index = 0, c,i,delka = 10;
+            char[] pole = new char[1000];
+
+            StreamReader sr = new StreamReader(zdroj);
+            StreamWriter sw = new StreamWriter(cil);
+
+            while ((c = sr.Read()) != -1)
+            {
+                if (char.IsLetter((char)c) || char.IsPunctuation((char)c))
+                {
+                    znak++;
+
+                    pole[index] = (char)c;
+                    index++;
+                    
+                    for(i = 0; i < znak ;i++)
+                    {
+                        if (pole[i] == (char)c && i !=index)
+                            znak--;
+
+                    }
+
+                }
+               
+            }
+            Console.WriteLine("V souboru se vyskytuje {0} znaku ", znak);
+            Console.ReadLine();
+            sr.Close(); sw.Close();
+        }
+        static void Main(string[] args)
+        {
+
+            JenomSlova5(@"c:\Users\lseme\source\repos\Uvod do programovaní\Soubory\SouborText.txt", @"c:\Users\lseme\source\repos\Uvod do programovaní\Soubory\PrevedenyText.txt");
+            Console.ReadLine();
+        }
+    }
+}
