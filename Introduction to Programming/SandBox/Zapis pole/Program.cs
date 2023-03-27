44a93cc3cd3288190097bcbf34bce353f26bfd72
SemanikCZ11 <128865957+SemanikCZ11@users.noreply.github.com>
2023-03-27 15:56:13 +0200

    Add files via upload

---
+++ Introduction to Programming/SandBox/Zapis pole/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace Zapis_pole
+{
+    class Program
+    {
+
+        static void JenomSlova5(string zdroj,string cil)
+        {
+            int delka = 0, c;
+            char[] pole = new char[100];
+
+            StreamReader sr = new StreamReader(zdroj);
+            StreamWriter sw = new StreamWriter(cil);
+
+            while ((c = sr.Read()) != -1)
+            {
+                if (char.IsLetter((char)c))
+                {
+                    pole[delka] = (char)c;
+                    delka++;
+                }
+                else
+                {
+
+                    if (delka >= 5)
+                        sw.WriteLine(pole, 0, delka);
+                        delka = 0;
+                    
+
+                }
+            }
+            sr.Close(); sw.Close();
+        }
+        static void Main(string[] args)
+        {
+
+
+            JenomSlova5(@"c:\Users\lseme\source\repos\Uvod do programovaní\Soubory\SouborText.txt", @"c:\Users\lseme\source\repos\Uvod do programovaní\Soubory\PrevedenyText.txt");
+            Console.ReadLine();
+
+        }
+    }
+}
