dd4571213835b80d9d99a4f259ed7336fe956b46
SemanikCZ11 <128865957+SemanikCZ11@users.noreply.github.com>
2023-03-27 15:46:46 +0200

    Add files via upload

---
+++ Introduction to Programming/SandBox/Ciferny soucet/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ciferny_soucet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int x = 0;
+            int y, cif = 0;
+
+            Console.WriteLine("Spocteme ciferny soucet zadaneho cisla");
+            Console.Write("Semn zadejte cislo  ");
+            y = x = Convert.ToInt32(Console.ReadLine());
+
+            while (y > 0)
+            {
+
+                cif = cif + y % 10;
+                y = y / 10;
+
+            }
+
+            Console.WriteLine("Ciferny soucet cisla {0} je {1}", x, cif);
+            Console.ReadLine();
+        }
+    }
+}
