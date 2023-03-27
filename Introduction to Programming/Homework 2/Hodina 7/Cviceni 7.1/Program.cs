5918fe01c6ad1c6edcd529611c054f100c127398
SemanikCZ11 <128865957+SemanikCZ11@users.noreply.github.com>
2023-03-27 18:41:52 +0200

    Add files via upload

---
+++ Introduction to Programming/Homework 2/Hodina 7/Cviceni 7.1/Program.cs
@@ -0,0 +1,43 @@
+﻿// Tento zdrojovy kod jsem vypracoval zcela samostatne bez cizi pomoci
+// Neokopiroval jsem ani neopsal jsem cizi zdrojove kody 
+// Nikdo me pri vypracovani neradil 
+// Pokud nektery radek porusuje toto pravidlo je oznacen komentarem
+// NENI MOJE TVORBA
+// Poruseni techto pravidel se povazuje za podvod, ktery lze potrestat VYLOUCENIM ZE STUDIA
+// Lukas Semecky 36991
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cviceni_7._1
+{
+    class Program
+    {
+        const int maxDel = 100;
+
+
+        static int [] NactiCislo(string s)
+        {
+            int i;
+            int [] pol = new int[maxDel];
+
+            for (i = 0; i < s.Length; i--)
+                pol[i] = s[s.Length - 1 - i] - '0';
+
+            return pol;
+
+        }
+        static void Main(string[] args)
+        {
+            int [] x;
+
+            string s = "5697853624125";
+
+            x = NactiCislo(s);
+            Console.WriteLine(x);
+            Console.ReadLine();
+        }
+    }
+}
