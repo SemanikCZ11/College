d97062fe82926cde777e8f749af21f54a416f0bd
SemanikCZ11 <128865957+SemanikCZ11@users.noreply.github.com>
2023-03-27 15:34:15 +0200

    Add files via upload

---
+++ MS Windows/Cviceni 3/Uloha 1/Program.cs
@@ -0,0 +1,49 @@
+﻿// Tento zdrojovy kod jsem vypracoval zcela samostatne bez cizi pomoci
+// Neokopiroval jsem ani neopsal jsem cizi zdrojove kody 
+// Nikdo me pri vypracovani neradil 
+// Pokud nektery radek porusuje toto pravidlo je oznacen komentarem
+// NENI MOJE TVORBA
+// Poruseni techto pravidel se povazuje za podvod, ktery lze potrestat VYLOUCENIM ZE STUDIA
+// Lukas Semecky 36991
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uloha_1
+{
+    class Program    // Uloha 1 Zadaní počtu zaměstnanců ,zadání jejich mzdy a vypsání mezd ktere jsou vetší než průměrná
+    {           
+        static void Main(string[] args)
+        {
+            Console.Write("Zadejte počet zaměstnancu: ");
+            int pocetlidi = Int32.Parse(Console.ReadLine());// Zadání počtu zaměstnancu od uživatele 
+
+            int[] firma = new int[pocetlidi];//nastavení velikosti pole - Firmy
+
+            int pmzda = 0;// Pro výpočet průměrné mzdy
+
+            for(int i=0;i<firma.Length;i++)//For cyklus pro přiřazení mezd zaměstnancům
+            {
+                Console.Write("Zadejte mzdu zaměstnance: ");
+                int mzda = Int32.Parse(Console.ReadLine());
+
+                firma[i] = mzda;// uložení mzdy zadané uživatelem do daného indexu
+                pmzda = pmzda + mzda;// sčítaní mezd
+            }
+
+            pmzda = pmzda / pocetlidi;// výpočet výše průměrné mzdy
+
+            Console.WriteLine("Průměrná mzda ve firmě je : {0}", pmzda);
+
+            foreach (int mzdy in firma)// cyklus foreach ktery projde pole a If větev pro vypsání jenom vetších mezd než průměrné
+            {
+
+                if (mzdy > pmzda) Console.WriteLine("Výše mezd vetší než průměrná {0}",mzdy);
+                else continue;
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MS Windows/Cviceni 3/Uloha 2/Program.cs
@@ -0,0 +1,37 @@
+﻿// Tento zdrojovy kod jsem vypracoval zcela samostatne bez cizi pomoci
+// Neokopiroval jsem ani neopsal jsem cizi zdrojove kody 
+// Nikdo me pri vypracovani neradil 
+// Pokud nektery radek porusuje toto pravidlo je oznacen komentarem
+// NENI MOJE TVORBA
+// Poruseni techto pravidel se povazuje za podvod, ktery lze potrestat VYLOUCENIM ZE STUDIA
+// Lukas Semecky 36991
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace Uloha_2
+{
+    class Program // Uloha 2 malá násobilka bez sudých výsledků 
+    {
+        static void Main(string[] args)
+        {
+            int vysledek = 0;
+    
+            for(int i=1;i<=10;i++)// První for cyklus pro proměnou kterou budeme násobit
+            {
+                if (i % 2 == 0) continue;// Pomocné If aby jsme se sudým číslem nemuseli procházet další cyklus for
+
+                for (int j = 1; j <= 10; j++)// Vnořený cyklus pro násobeni
+                {
+                    vysledek = i * j;
+                    if (vysledek % 2 != 0) Console.WriteLine("{0} * {1} = {2}", i, j,vysledek);//Pokud výsledek není sudý zapíšeme na konzoli
+                    else continue;
+                }
+                
+            }
+            Console.ReadKey();
+        }
+    }
+}
